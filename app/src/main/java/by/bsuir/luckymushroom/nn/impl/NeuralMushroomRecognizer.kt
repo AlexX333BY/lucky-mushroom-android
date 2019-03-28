b0343352de6be66ea6c9fc18c99d777ee111c450
Aliaksei <alexx333by@protonmail.com>
2019-03-29 00:10:18 +0300

    Converting bitmap to buffer implemented

--- app/src/main/java/by/bsuir/luckymushroom/nn/impl/NeuralMushroomRecognizer.kt
+++ app/src/main/java/by/bsuir/luckymushroom/nn/impl/NeuralMushroomRecognizer.kt
@@ -4,12 +4,45 @@ import by.bsuir.luckymushroom.nn.common.MushroomRecognizer
 import by.bsuir.luckymushroom.nn.common.RecognitionResult
 import java.io.File
 import org.tensorflow.lite.Interpreter
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import java.io.FileInputStream
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 class NeuralMushroomRecognizer(modelAndWeights: File) : MushroomRecognizer {
     override fun recognize(image: File): Array<RecognitionResult> {
-        TODO("parse output")
+        val labelProb = arrayOf(ByteArray(2), ByteArray(2))
+
+        val bitmap = BitmapFactory.decodeStream(FileInputStream(image))
+        neuralNetwork.run(convertBitmapToByteBuffer(bitmap), labelProb)
+        TODO(
+                "1. convert image to ResNet's 224 * 224" +
+                "2. parse output" +
+                "3. properly get labels"
+        )
     }
 
-    protected val CHANNELS = 1;
+    protected fun convertBitmapToByteBuffer(bitmap: Bitmap): ByteBuffer {
+        val imageBytes = ByteBuffer.allocateDirect(bitmap.byteCount)
+        imageBytes.order(ByteOrder.nativeOrder())
+
+        val pixels = IntArray(bitmap.height * bitmap.width)
+        bitmap.getPixels(pixels, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+
+        var pixel = 0
+        var value: Int
+        for (i in 0 until bitmap.width) {
+            for (j in 0 until bitmap.height) {
+                value = pixels[pixel++]
+                imageBytes.put(((value shr 16) and 0xFF).toByte())
+                imageBytes.put(((value shr 8) and 0xFF).toByte())
+                imageBytes.put((value and 0xFF).toByte())
+            }
+        }
+
+        return imageBytes
+    }
+
     protected val neuralNetwork: Interpreter = Interpreter(modelAndWeights)
 }
