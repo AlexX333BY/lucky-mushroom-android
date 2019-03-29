4e734e635ef4dbd8f0943f8b9c40b1024dfedf2d
Aliaksei <alexx333by@protonmail.com>
2019-03-29 13:32:14 +0300

    Replaced ByteBuffer with float array

--- app/src/main/java/by/bsuir/luckymushroom/nn/impl/NeuralMushroomRecognizer.kt
+++ app/src/main/java/by/bsuir/luckymushroom/nn/impl/NeuralMushroomRecognizer.kt
@@ -7,16 +7,14 @@ import org.tensorflow.lite.Interpreter
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import java.io.FileInputStream
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
 
 class NeuralMushroomRecognizer(modelAndWeights: File) : MushroomRecognizer {
     protected val HEIGHT = 224 // better to parse from some JSON?
     protected val WIDTH = 224  // better to parse from some JSON?
-    protected val PIXEL_SIZE = 4
+    protected val PIXEL_SIZE = 3
 
     override fun recognize(image: File): Array<RecognitionResult> {
-        val labelProb = arrayOf(ByteArray(2), ByteArray(2))
+        val labelProb = arrayOf(ByteArray(1), ByteArray(1))
 
         val bitmap = Bitmap.createScaledBitmap(BitmapFactory.decodeStream(FileInputStream(image)), WIDTH, HEIGHT, false)
 
@@ -28,25 +26,27 @@ class NeuralMushroomRecognizer(modelAndWeights: File) : MushroomRecognizer {
         )
     }
 
-    protected fun convertBitmapToByteBuffer(bitmap: Bitmap): ByteBuffer {
-        val imageBytes = ByteBuffer.allocateDirect(bitmap.byteCount)
-        imageBytes.order(ByteOrder.nativeOrder())
+    protected fun convertBitmapToByteBuffer(bitmap: Bitmap): Array<Array<FloatArray>> {
+        if ((bitmap.width != WIDTH) || (bitmap.height != HEIGHT)) {
+            throw IllegalArgumentException(String.format("Bitmap size should be of %dx%d size", WIDTH, HEIGHT))
+        }
 
-        val pixels = IntArray(bitmap.height * bitmap.width)
-        bitmap.getPixels(pixels, 0, bitmap.width, 0, 0, bitmap.width, bitmap.height)
+        val floatedImage = Array(WIDTH) { Array(HEIGHT) { FloatArray(PIXEL_SIZE) } }
+        val pixels = IntArray(WIDTH * HEIGHT)
+        bitmap.getPixels(pixels, 0, WIDTH, 0, 0, WIDTH, HEIGHT)
 
-        var pixel = 0
-        var value: Int
-        for (i in 0 until bitmap.width) {
-            for (j in 0 until bitmap.height) {
-                value = pixels[pixel++]
-                imageBytes.put(((value shr 16) and 0xFF).toByte())
-                imageBytes.put(((value shr 8) and 0xFF).toByte())
-                imageBytes.put((value and 0xFF).toByte())
+        var currentPixel = 0
+        var currentPixelValue: Int
+        for (i in 0 until WIDTH) {
+            for (j in 0 until HEIGHT) {
+                currentPixelValue = pixels[currentPixel++]
+                floatedImage[i][j][0] = ((currentPixelValue shr 16) and 0xFF).toFloat()
+                floatedImage[i][j][1] = ((currentPixelValue shr 8) and 0xFF).toFloat()
+                floatedImage[i][j][2] = (currentPixelValue and 0xFF).toFloat()
             }
         }
 
-        return imageBytes
+        return floatedImage
     }
 
     protected val neuralNetwork: Interpreter = Interpreter(modelAndWeights)
