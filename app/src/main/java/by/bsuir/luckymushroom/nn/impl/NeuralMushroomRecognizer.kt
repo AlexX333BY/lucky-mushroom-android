c35945838d90d94d7bdca4a01ee9c8244d34ddcf
Aliaksei <alexx333by@protonmail.com>
2019-03-31 20:24:22 +0300

    Recognition implemented

--- app/src/main/java/by/bsuir/luckymushroom/nn/impl/NeuralMushroomRecognizer.kt
+++ app/src/main/java/by/bsuir/luckymushroom/nn/impl/NeuralMushroomRecognizer.kt
@@ -3,50 +3,49 @@ package by.bsuir.luckymushroom.nn.impl
 import by.bsuir.luckymushroom.nn.common.MushroomRecognizer
 import by.bsuir.luckymushroom.nn.common.RecognitionResult
 import java.io.File
-import org.tensorflow.lite.Interpreter
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import java.io.FileInputStream
 
 class NeuralMushroomRecognizer(modelAndWeights: File) : MushroomRecognizer {
-    protected val HEIGHT = 224 // better to parse from some JSON?
-    protected val WIDTH = 224  // better to parse from some JSON?
+    protected val HEIGHT = 224
+    protected val WIDTH = 224
     protected val PIXEL_SIZE = 3
-    protected val LABELS_COUNT = 2 // class count
+
+    protected val CLASS_COUNT = 1000
+    protected val INPUT_NODE_NAME = "input_1"
+    protected val OUTPUT_NODE_NAME = "output"
 
     override fun recognize(image: File): Array<RecognitionResult> {
-        val probabilities = Array(1) { FloatArray(LABELS_COUNT) }
+        val probabilities = FloatArray(CLASS_COUNT)
         val bitmap = Bitmap.createScaledBitmap(BitmapFactory.decodeStream(FileInputStream(image)), WIDTH, HEIGHT, false)
 
-        neuralNetwork.run(convertBitmapToFloatArray(bitmap), probabilities)
-        TODO(
-                "1. make it not throw exception" +
-                "2. parse output"
-        )
+        neuralNetwork.feed(INPUT_NODE_NAME, convertBitmapToFloatArray(bitmap),
+            1, WIDTH.toLong(), HEIGHT.toLong(), PIXEL_SIZE.toLong())
+        neuralNetwork.run(arrayOf(OUTPUT_NODE_NAME))
+        neuralNetwork.fetch(OUTPUT_NODE_NAME, probabilities)
+
+        TODO("Load labels & parse output")
     }
 
-    protected fun convertBitmapToFloatArray(bitmap: Bitmap): Array<Array<FloatArray>> {
+    protected fun convertBitmapToFloatArray(bitmap: Bitmap): FloatArray {
         if ((bitmap.width != WIDTH) || (bitmap.height != HEIGHT)) {
             throw IllegalArgumentException(String.format("Bitmap size should be of %dx%d size", WIDTH, HEIGHT))
         }
 
-        val floatedImage = Array(WIDTH) { Array(HEIGHT) { FloatArray(PIXEL_SIZE) } }
+        val floatedImage = FloatArray(WIDTH * HEIGHT * 3)
         val pixels = IntArray(WIDTH * HEIGHT)
         bitmap.getPixels(pixels, 0, WIDTH, 0, 0, WIDTH, HEIGHT)
 
-        var currentPixel = 0
-        var currentPixelValue: Int
-        for (i in 0 until WIDTH) {
-            for (j in 0 until HEIGHT) {
-                currentPixelValue = pixels[currentPixel++]
-                floatedImage[i][j][0] = ((currentPixelValue shr 16) and 0xFF).toFloat()
-                floatedImage[i][j][1] = ((currentPixelValue shr 8) and 0xFF).toFloat()
-                floatedImage[i][j][2] = (currentPixelValue and 0xFF).toFloat()
-            }
+        for ((index, pixel) in pixels.withIndex()) {
+            floatedImage[PIXEL_SIZE * index] = ((pixel shr 16) and 0xFF).toFloat()
+            floatedImage[PIXEL_SIZE * index + 1] = ((pixel shr 8) and 0xFF).toFloat()
+            floatedImage[PIXEL_SIZE * index + 2] = (pixel and 0xFF).toFloat()
         }
 
         return floatedImage
     }
 
-    protected val neuralNetwork: Interpreter = Interpreter(modelAndWeights)
+    protected val neuralNetwork: TensorFlowInferenceInterface = TensorFlowInferenceInterface(FileInputStream(modelAndWeights))
 }
